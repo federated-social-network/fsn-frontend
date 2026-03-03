5741a2442d0d30c2a719509bf9fa9640b0011f0f
Harish1604 <harish16042005@gmail.com>
2026-03-04 00:33:50 +0530

    like clr

--- src/main.tsx
+++ src/main.tsx
@@ -10,8 +10,8 @@ import { HelmetProvider } from 'react-helmet-async'
  */
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HelmetProvider>
+    
       <App />
-    </HelmetProvider>
+    
   </StrictMode>,
 )
